302ecba435faa5d0ac30198e7671b3dd8c8fde59
dangductri2712 <139195338+dangductri2712@users.noreply.github.com>
2023-07-12 14:06:45 +0700

    Update index.js. Now add react-routers-dom module

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-
-//import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import React, {useRef,useState} from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 
-import Body from './body';
+import Body from './body.js';
+import Skills from './skills.js';
+import Interests from './interest.js';
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 const domNode = document.getElementById('root');
 
 const Structure = ()=>{
@@ -13,7 +16,15 @@ const Structure = ()=>{
   const educationRef = useRef(null);
   return(
   <div>
-    {Body()} 
+    <BrowserRouter>
+      <Routes>
+        <Route index element = {<Body/>}/>
+        <Route path = "/body" element = {<Body/>}></Route>
+        <Route path = "/interests" element = {<Interests/>}></Route>
+        <Route path = "/skills" element = {<Skills/>}></Route>
+        <Route path = "/education" element = {<Body/>}></Route>
+      </Routes>
+    </BrowserRouter>
   </div>
 )
 }
